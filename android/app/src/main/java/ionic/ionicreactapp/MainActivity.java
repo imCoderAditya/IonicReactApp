24fb648067614169e9f4675bbb6730000fb81b52
Aditya sharma <adsharmavashishtha@gmail.com>
2023-09-27 03:32:50 +0530

    First Commit

---
+++ android/app/src/main/java/ionic/ionicreactapp/MainActivity.java
@@ -0,0 +1,5 @@
+package ionic.ionicreactapp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -1,11 +1,18 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import { useParams } from 'react-router';
-import ExploreContainer from '../components/ExploreContainer';
-import './Page.css';
+import {
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import { useParams } from "react-router";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Page.css";
 
 const Page: React.FC = () => {
-
-  const { name } = useParams<{ name: string; }>();
+  const { name } = useParams<{ name: string }>();
 
   return (
     <IonPage>
@@ -14,7 +21,7 @@ const Page: React.FC = () => {
           <IonButtons slot="start">
             <IonMenuButton />
           </IonButtons>
-          <IonTitle>{name}</IonTitle>
+          <IonTitle>{"Home"}</IonTitle>
         </IonToolbar>
       </IonHeader>
 
